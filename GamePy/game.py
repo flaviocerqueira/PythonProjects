1a9600d596df323a244cc6c110a210c4fe560d5b
Flavio <fsilvacerqueira@gmail.com>
2022-03-15 21:11:16 -0300

    'upload'

---
+++ GamePy/game.py
@@ -0,0 +1,13 @@
+from models.calcular import Calcular
+
+def main() -> None:
+    pontos: int = 0
+    jogar(pontos)
+
+
+def jogar(pontos: int) -> None:
+    pass
+
+
+if __name__ == '__main__':
+    main()
